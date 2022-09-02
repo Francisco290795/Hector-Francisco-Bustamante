a65883cfa553c09a3e9e8eb3acd3cdeb26074d5d
Francisco290795 <112668670+Francisco290795@users.noreply.github.com>
2022-09-01 21:08:17 -0300

    Add files via upload

---
+++ Entrega1-Bustamante Hector Francisco.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE  `TTC`  DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_es_0900_ai_ci 
+DEFAULT ENCRYPTION='N' ;
+ 
+USE TTC;
+
+create table Empleados (
+Id_empleado int primary key auto_increment not null ,
+Nombre varchar (30),
+Direccion varchar (30),
+Telefono int ,
+Mail varchar (30) 
+);
+
+create table Proveedores (
+Id_proveedor int primary key auto_increment not null ,
+Nombre varchar (30),
+Direccion varchar (30),
+Telefono int ,
+Mail varchar (30) 
+);
+
+create table Categorias (
+Id_categoria int primary key auto_increment not null ,
+Nombre varchar (30)
+);
+
+ create table Productos (
+ Id_producto int  primary key auto_increment not null ,
+ Nombre 	varchar (30),
+ Descripcion varchar (30),
+ Marca	varchar (30),
+ Precio decimal (1.2) ,
+ Id_proveedor int not null,
+ Id_categoria int not null,
+ foreign key (Id_proveedor) references Proveedores (Id_proveedor),
+ foreign key (Id_categoria) references Categorias (Id_categoria)
+ );
+ 
+ create table Ventas (
+Id_venta int primary key auto_increment not null ,
+Fecha datetime ,
+Total decimal (1.2),
+Id_producto int,
+Id_empleado int,
+foreign key (Id_producto) references Productos (Id_producto),
+foreign key (Id_empleado) references Empleados (Id_empleado)
+)
